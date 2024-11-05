8e506314d54633a166e9317e6f5fa9773738fd9e
vxyi <vxyi@qq.com>
2024-11-05 17:17:42 +0800

    给 SPADE 也换上了 MLE, 直接做减法被重命名为 simple 方法, DI 的 ROI 貌似需要按 4sigma 原则

--- src/estimator.py
+++ src/estimator.py
@@ -57,10 +57,9 @@ def freq_estimator(sample: np.ndarray, sampling_rate=1, method='lse'):
 
 
 
-
-def di_td_estimator(sample: np.ndarray, w = None, method='mle'):
+def td_estimator(measurement, sample: np.ndarray, w = None, method='mle'):
         from .core import DI, qCMOS
-        
+
         origin_shape = sample.shape
 
         detectors = origin_shape[-1]
@@ -69,22 +68,34 @@ def di_td_estimator(sample: np.ndarray, w = None, method='mle'):
         flatten_data = sample.reshape(-1, detectors)
 
         if method.lower() == 'simple':
-            temp = flatten_data / flatten_data.sum(axis=-1).reshape(-1, 1)
-            x_axis = np.arange(detectors)
-            time_domain = (temp @ x_axis)
+
+            if measurement.lower() == 'di':
+                temp = flatten_data / flatten_data.sum(axis=-1).reshape(-1, 1)
+                x_axis = np.arange(detectors)
+                time_domain = (temp @ x_axis)
+
+            if measurement.lower() == 'spade':
+                time_domain = sample[..., 1] - sample[..., 0]
+
 
         elif method.lower() == 'mle':
-            def _p(x, s, w):
-                _sigma = DI.SIGMA / qCMOS.PIXEL_SIZE
-                return (1-w)/np.sqrt(tau*_sigma**2)*np.exp(-(x-s)**2/(2*_sigma**2)) + w/detectors
-            
-            def _negative_ll(data, w):
-                x_axis = np.arange(detectors)
-                return lambda s: - data.T @ np.log(_p(x_axis, s, w)) / data.sum()
-            
+            _sigma = DI.SIGMA / qCMOS.PIXEL_SIZE
+
+            if measurement.lower() == 'di':
+                def _p(x, s, w):
+                    return (1-w)/np.sqrt(tau*_sigma**2)*np.exp(-(x-s)**2/(2*_sigma**2)) + w/detectors
+
+            elif measurement.lower() == 'spade':
+                def _p(k, s, w):
+                    return (1-w)/8*np.exp(-(s/(2*_sigma))**2)*(s/_sigma+2*(-1)**(k+1))**2 + w/detectors
+
+            def _nll(data, w):
+                axis = np.arange(detectors)
+                return lambda s: - data.T @ np.log(_p(axis, s, w)) / data.sum()
+
             def _run_mle(data, w):
                 # Use BFGS algorithm to minimize negative log-likelihood function.
-                result = minimize(_negative_ll(data, w), x0=detectors/2, method='BFGS')
+                result = minimize(_nll(data, w), x0=detectors/2, method='BFGS')
                 
                 if result.success:
                     return result.x[0]
@@ -98,3 +109,50 @@ def di_td_estimator(sample: np.ndarray, w = None, method='mle'):
                 time_domain = [_run_mle(flatten_data[i], w_set[i]) for i in range(works)]  
 
         return np.array(time_domain).reshape(*origin_shape[:-1])
+
+
+
+
+
+
+
+# def di_td_estimator(sample: np.ndarray, w = None, method='mle'):
+#         from .core import DI, qCMOS
+        
+#         origin_shape = sample.shape
+
+#         detectors = origin_shape[-1]
+#         works = np.prod(origin_shape[:-1])
+
+#         flatten_data = sample.reshape(-1, detectors)
+
+#         if method.lower() == 'simple':
+#             temp = flatten_data / flatten_data.sum(axis=-1).reshape(-1, 1)
+#             x_axis = np.arange(detectors)
+#             time_domain = (temp @ x_axis)
+
+#         elif method.lower() == 'mle':
+#             def _p(x, s, w):
+#                 _sigma = DI.SIGMA / qCMOS.PIXEL_SIZE
+#                 return (1-w)/np.sqrt(tau*_sigma**2)*np.exp(-(x-s)**2/(2*_sigma**2)) + w/detectors
+            
+#             def _negative_ll(data, w):
+#                 x_axis = np.arange(detectors)
+#                 return lambda s: - data.T @ np.log(_p(x_axis, s, w)) / data.sum()
+            
+#             def _run_mle(data, w):
+#                 # Use BFGS algorithm to minimize negative log-likelihood function.
+#                 result = minimize(_negative_ll(data, w), x0=detectors/2, method='BFGS')
+                
+#                 if result.success:
+#                     return result.x[0]
+#                 else:
+#                     raise RuntimeError('not converged')
+            
+#             if w is None:
+#                 time_domain = [_run_mle(flatten_data[i], 0) for i in range(works)]
+#             else:
+#                 w_set = np.ravel(w)
+#                 time_domain = [_run_mle(flatten_data[i], w_set[i]) for i in range(works)]  
+
+#         return np.array(time_domain).reshape(*origin_shape[:-1])
