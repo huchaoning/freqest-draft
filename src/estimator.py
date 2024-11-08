19b56cdfa56eabcd2834106af97c04fcb72025fb
vxyi <vxyi@qq.com>
2024-11-08 21:46:34 +0800

    最大似然估计有可能会造成估计值的方差小于 CRB, 尤其是在光子数比较少的情况下. 所以 SPADE 换回了 SUBSTRACT 算法

--- src/estimator.py
+++ src/estimator.py
@@ -8,7 +8,7 @@ __all__ = ['freq_estimator', 'td_estimator']
 
 def freq_estimator(sample: np.ndarray, sampling_rate=1, method='lse'):
     # Standardize sample
-    sample = (sample - sample.mean()) / sample.std()
+    # sample = (sample - sample.mean()) / sample.std()
 
     def waveform(n, f):
         return np.sin(tau * f * n / sampling_rate)
@@ -44,6 +44,8 @@ def freq_estimator(sample: np.ndarray, sampling_rate=1, method='lse'):
 
 
 
+
+
 def td_estimator(measurement, sample: np.ndarray, w=None, method='mle'):
         from .core import DI, qCMOS
 
@@ -54,46 +56,107 @@ def td_estimator(measurement, sample: np.ndarray, w=None, method='mle'):
 
         flatten_data = sample.reshape(-1, detectors)
 
-        if method.lower() == 'simple':
 
-            if measurement.lower() == 'di':
+        if measurement.lower() == 'spade':
+            time_domain = sample[..., 1] - sample[..., 0]
+
+
+        elif measurement.lower() == 'di':
+
+            if method.lower() == 'simple':
                 temp = flatten_data / flatten_data.sum(axis=-1).reshape(-1, 1)
                 x_axis = np.arange(detectors)
                 time_domain = (temp @ x_axis)
 
-            if measurement.lower() == 'spade':
-                time_domain = sample[..., 1] - sample[..., 0]
-
-
-        elif method.lower() == 'mle':
-            _sigma = DI.SIGMA / qCMOS.PIXEL_SIZE
-
-            if measurement.lower() == 'di':
+            elif method.lower() == 'mle':
                 def _p(x, s, w):
+                    _sigma = DI.SIGMA / qCMOS.PIXEL_SIZE
                     return (1-w)/np.sqrt(tau*_sigma**2)*np.exp(-(x-s)**2/(2*_sigma**2)) + w/detectors
 
-            elif measurement.lower() == 'spade':
-                def _p(k, s, w):
-                    return (1-w)/8*np.exp(-(s/(2*_sigma))**2)*(s/_sigma+2*(-1)**(k+1))**2 + w/detectors
+                def _nll(data, w):
+                    axis = np.arange(detectors)
+                    return lambda s: - data.T @ np.log(_p(axis, s, w)) / data.sum()
 
-            def _nll(data, w):
-                axis = np.arange(detectors)
-                return lambda s: - data.T @ np.log(_p(axis, s, w)) / data.sum()
-
-            def _run_mle(data, w):
-                # Use BFGS algorithm to minimize negative log-likelihood function.
-                result = minimize(_nll(data, w), x0=detectors/2, method='BFGS')
+                def _run_mle(data, w):
+                    # Use BFGS algorithm to minimize negative log-likelihood function.
+                    result = minimize(_nll(data, w), x0=detectors/2, method='BFGS')
+                    
+                    if result.success:
+                        return result.x[0]
+                    else:
+                        raise RuntimeError('not converged')
                 
-                if result.success:
-                    return result.x[0]
+                if w is None:
+                    time_domain = [_run_mle(flatten_data[i], 0) for i in range(works)]
                 else:
-                    raise RuntimeError('not converged')
-            
-            if w is None:
-                time_domain = [_run_mle(flatten_data[i], 0) for i in range(works)]
-            else:
-                w_set = np.ravel(w)
-                time_domain = [_run_mle(flatten_data[i], w_set[i]) for i in range(works)]  
+                    w_set = np.ravel(w)
+                    time_domain = [_run_mle(flatten_data[i], w_set[i]) for i in range(works)]
 
-        return np.array(time_domain).reshape(*origin_shape[:-1])
+
+        time_domain = np.array(time_domain)
+        std = time_domain.std()
+        mean = time_domain.mean()
+
+        return ((time_domain - mean) / std).reshape(*origin_shape[:-1])
+
+
+
+
+
+# def td_estimator(measurement, sample: np.ndarray, w=None, method='mle'):
+#         from .core import DI, qCMOS
+
+#         origin_shape = sample.shape
+
+#         detectors = origin_shape[-1]
+#         works = np.prod(origin_shape[:-1])
+
+#         flatten_data = sample.reshape(-1, detectors)
+
+#         if method.lower() == 'simple':
+
+#             if measurement.lower() == 'di':
+#                 temp = flatten_data / flatten_data.sum(axis=-1).reshape(-1, 1)
+#                 x_axis = np.arange(detectors)
+#                 time_domain = (temp @ x_axis)
+
+#             if measurement.lower() == 'spade':
+#                 time_domain = sample[..., 1] - sample[..., 0]
+
+
+#         elif method.lower() == 'mle':
+#             if measurement.lower() == 'di':
+#                 def _p(x, s, w):
+#                     _sigma = DI.SIGMA / qCMOS.PIXEL_SIZE
+#                     return (1-w)/np.sqrt(tau*_sigma**2)*np.exp(-(x-s)**2/(2*_sigma**2)) + w/detectors
+
+#             elif measurement.lower() == 'spade':
+#                 def _p(k, s, w):
+#                     return (1-w)/8*np.exp(-(s/(2*DI.SIGMA))**2)*(s/DI.SIGMA+2*(-1)**(k+1))**2 + w/detectors
+
+#             def _nll(data, w):
+#                 axis = np.arange(detectors)
+#                 return lambda s: - data.T @ np.log(_p(axis, s, w)) / data.sum()
+
+#             def _run_mle(data, w):
+#                 # Use BFGS algorithm to minimize negative log-likelihood function.
+#                 result = minimize(_nll(data, w), x0=detectors/2, method='BFGS')
+                
+#                 if result.success:
+#                     return result.x[0]
+#                 else:
+#                     raise RuntimeError('not converged')
+            
+#             if w is None:
+#                 time_domain = [_run_mle(flatten_data[i], 0) for i in range(works)]
+#             else:
+#                 w_set = np.ravel(w)
+#                 time_domain = [_run_mle(flatten_data[i], w_set[i]) for i in range(works)]
+
+#         time_domain = np.array(time_domain)
+
+#         std = time_domain.std()
+#         mean = time_domain.mean()
+
+#         return ((time_domain - mean) / std).reshape(*origin_shape[:-1])
 
