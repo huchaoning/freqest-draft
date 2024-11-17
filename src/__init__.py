d539875d4126b8c978ed529aa554b9a63378d904
vxyi <vxyi@qq.com>
2024-11-17 15:39:50 +0800

    直接在src里 import 常用的库

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,30 @@
+import matplotlib.pyplot as plt
+
+from mpl_toolkits.axes_grid1.inset_locator import mark_inset
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+
+from matplotlib_inline import backend_inline
+backend_inline.set_matplotlib_formats('svg')
+del backend_inline
+
+from matplotlib.font_manager import fontManager
+fontManager.addfont('src/lmroman10.otf')
+del fontManager
+
+plt.rcParams['font.family'] = 'Latin Modern Roman'
+plt.rcParams['mathtext.fontset'] = 'cm'
+
+
+import numpy as np
+from tqdm import tqdm
+from time import sleep
+import os
+
+from datetime import datetime
+today = datetime.strftime(datetime.today(), '%Y%m%d')
+del datetime
+
+
 from .core import *
 from .estimator import *
 
@@ -6,3 +33,5 @@ from .estimator import *
 import platform
 if platform.system() == 'Windows':
     from .controller import *
+
+
