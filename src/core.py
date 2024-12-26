220d65c14cdd38ea84238d6452a1527417b0365a
vxyi <vxyi@qq.com>
2024-12-26 15:56:31 +0800

    fisherinfo

--- src/core.py
+++ src/core.py
@@ -22,9 +22,6 @@ __all__ = [
     'LoadEstimates',
     'Estimation',
 
-    'FisherInformation',
-    'ApproxFisherInformation',
-
     'Simulator'
 ]
 
@@ -311,33 +308,6 @@ class Estimation:
         expt.est_all()
         return expt.estimates
 
-
-
-
-######################
-#     FI and CRB     #
-######################
-def FisherInformation(A_list: np.ndarray, freq_list: np.ndarray, sigma=_Share.SIGMA, N=50):
-    results_1, results_2 = [], []
-    n = np.arange(N)
-    for A in A_list:
-        for f in freq_list:
-            _temp = ((tau*n) * np.cos(tau*f*n))**2
-            results_1.append((A/sigma)**2 * _temp.sum())
-        results_2.append(results_1)
-    return np.array(results_2)
-
-def ApproxFisherInformation(A_list: np.ndarray, sigma=_Share.SIGMA, N=50):
-    n = np.arange(N)
-    results = [2*(A*pi/sigma)**2 * (n**2).sum() for A in A_list]
-    return np.array(results)
-
-def VelocityFisherInformationMatrix(sampling_rate=20, sigma=_Share.SIGMA, N=10):
-    t = np.arange(N)/sampling_rate
-    fi11 = 1/sigma**2 * np.sum(t**2)
-    fi12 = 1/sigma**2 * np.sum(t**1)
-    fi22 = 1/sigma**2 * np.sum(t**0)
-    return np.array([[fi11, fi12], [fi12, fi22]])
 
 
 
